c2e5225ad70111352b9150094f541d1e05573815
hosseinkhashaypour <154625781+hosseinkhashaypour@users.noreply.github.com>
2024-06-15 11:41:54 +0330

    Add files via upload

--- login.js
+++ login.js
@@ -2,13 +2,18 @@
 const usernameInput = document.getElementById("usernameInput")
 const passwordInput = document.getElementById("passwordInput")
 // console.log(passwordInput);
-
-function checkpassword (){
-    if (usernameInput.value === "hosseinkhashaypour" && passwordInput.value === "hkh13850@") {
+function checkpassword() {
+    if (usernameInput.value === "h20062006kh@gmail.com" && passwordInput.value === "hkh13850@") {
+        localStorage.setItem("coderDash" , usernameInput.value)
         window.location.href = "index.html"
+    }
+    else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong! Check user and password",
+            // footer: '<a href="#">Why do I have this issue?</a>'
+        });
     }
     return
 }
-
-checkpassword()
-
---
+++ script.js
@@ -0,0 +1,44 @@
+let coderUsername = localStorage.getItem("coderDash")
+let greetTocoder = `Hello dear ${coderUsername}`
+document.title = greetTocoder
+const menuMobile = document.querySelector(".menu-mobile")
+
+const themeIcon = document.querySelector(".light-mode")
+// console.log(themeIcon)
+
+const menyOverlay = document.querySelector('.overlay')
+
+function applyTheme(theme) {
+    if (theme === "dark") {
+        document.body.classList.remove("light-theme")
+        document.body.classList.add("dark-theme")
+        themeIcon.src = "assets/images/light-mode.png";
+        menuMobile.style.color = "white"
+        menyOverlay.style.backgroundColor = "#eee"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+        document.body.classList.add("light-theme")
+        themeIcon.src = "assets/images/dark-mode.png";
+        menuMobile.style.color = "black"
+        menyOverlay.style.backgroundColor = "#151515"
+    }
+}
+themeIcon.addEventListener('click', () => {
+    if (document.body.classList.contains("dark-theme")) {
+        localStorage.setItem('theme', 'light');
+        applyTheme('light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+        applyTheme('dark');
+    }
+});
+
+
+function openNav(){
+    document.getElementById("myNav").style.width = "80%"
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = "0"
+}
